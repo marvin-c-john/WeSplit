fec3bcf624c346da057a2c680fd0531091900aa1
Marvin John <Marvinc.john.03@gmail.com>
2024-08-04 01:37:26 +0200

    [Bug] Fix Picker not working and add functions to calculate bill

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var billAmount = ""
-    @State private var selectedPeople = "2 people"
+    @State private var billAmount: Double = 0
+    @State private var selectedPeople = 2
     @State private var selectedTip = 10
-    @State private var total = 0.00
+    @State private var totalBill = 0.00
+    @State private var amountPerPerson = 0.00
     
     var people = [2, 3, 4, 5]
-    var tips = [10, 15, 20, 25, 0]
+    var tips: [Int] = [10, 15, 20, 25, 0]
     
     var body: some View {
         NavigationView{
             Form{
-                TextField("Enter Bill Amount", text: $billAmount)
+                TextField("Enter Bill Amount", value: $billAmount, format: .number)
                 
                 Picker("Number of people", selection: $selectedPeople) {
                     ForEach(people, id: \.self){
@@ -29,7 +30,7 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Picker("", selection: $selectedTip) {
+                    Picker("Select tip", selection: $selectedTip) {
                         ForEach(tips, id: \.self){
                             Text("\($0)%")
                         }
@@ -38,15 +39,34 @@ struct ContentView: View {
                 } header: {
                     Text("How much tip would you like to leave?")
                 }
+               
                 
                 
                 Section(header: Text("Total Bill")) {
-                    TextField("$\(total)", value: $total, format: .number)
+                    TextField("$\(totalBill)", value: $totalBill, format: .number)
+                        .disabled(true)
+                }
+                
+                
+                Section(header: Text("Amount per Person")) {
+                    TextField("$\(amountPerPerson)", value: $amountPerPerson, format: .number)
                         .disabled(true)
                 }
             }
             .navigationTitle("WeSplit")
+            .onSubmit {
+               totalBill = getTotalBill(amount: billAmount, percentage: Double(selectedTip))
+                amountPerPerson = getAmountPerPerson(totalBill: totalBill)
+            }
         }
+    }
+    
+    func getTotalBill(amount: Double, percentage: Double) -> Double{
+        return (amount * percentage) / 100
+    }
+    
+    func getAmountPerPerson(totalBill: Double) -> Double{
+        totalBill / Double(selectedPeople)
     }
 }
 
